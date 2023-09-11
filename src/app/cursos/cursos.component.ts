036e561c8472e4fe313ae649c165a713f738e200
luanpoppe <luanpoppe@gmail.com>
2023-09-11 17:07:31 -0300

    fim do módulo 1

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.scss']
+  styleUrls: ['./cursos.component.scss'],
 })
 export class CursosComponent implements OnInit {
+  nomePortal: string;
+  cursos: string[];
 
-  constructor() { }
+  constructor(private CursosService: CursosService) {
+    this.nomePortal = 'http.loiane.training';
 
-  ngOnInit(): void {
+    this.cursos = this.CursosService.getCursos();
   }
 
+  ngOnInit(): void {}
 }
