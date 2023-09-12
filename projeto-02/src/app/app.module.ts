4842f17092030398fa6399e7671687b44c22308d
luanpoppe <luanpoppe@gmail.com>
2023-09-12 15:52:28 -0300

    terminei a aula 02

--- projeto-02/src/app/app.module.ts
+++ projeto-02/src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { routing } from './app.routing';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CursosComponent, HomeComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule, routing],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
