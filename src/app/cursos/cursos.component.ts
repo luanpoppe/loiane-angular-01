48ab92d2e6a0f24f48a8d169ca1fc998bde0adeb
luanpoppe <luanpoppe@gmail.com>
2023-09-13 13:49:19 -0300

    Terminei a aula '08 - Rotas: Definindo e extraindo parâmetros de url (query)'

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,16 +1,44 @@
+import { ActivatedRoute, Router } from '@angular/router';
 import { CursosService } from './cursos.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.scss'],
 })
-export class CursosComponent implements OnInit {
+export class CursosComponent implements OnInit, OnDestroy {
   cursos?: any[];
-  constructor(private CursosService: CursosService) {}
+  pagina?: number;
+  inscricao?: Subscription;
+
+  constructor(
+    private CursosService: CursosService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.cursos = this.CursosService.getCursos();
+
+    this.inscricao = this.route.queryParams.subscribe((queryParams: any) => {
+      this.pagina = queryParams['pagina'];
+    });
+  }
+
+  ngOnDestroy() {
+    this.inscricao?.unsubscribe();
+  }
+
+  proximaPagina() {
+    if (this.pagina) {
+      this.pagina++;
+    }
+    this.router.navigate(['cursos'], {
+      queryParams: {
+        pagina: this.pagina,
+      },
+    });
   }
 }
