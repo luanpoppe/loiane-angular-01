69a3a6e17258f58b1f7ba33eb26f2812f5c3e2b8
luanpoppe <luanpoppe@gmail.com>
2023-09-13 10:40:13 -0300

    terminei a aula 'Rotas: Definindo e extraindo parâmetros de roteamento'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { CursosComponent } from './cursos/cursos.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { routing } from './app.routing';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 @NgModule({
-  declarations: [AppComponent, CursosComponent, HomeComponent, LoginComponent],
+  declarations: [AppComponent, CursosComponent, HomeComponent, LoginComponent, CursoDetalheComponent],
   imports: [BrowserModule, AppRoutingModule, routing],
   providers: [],
   bootstrap: [AppComponent],
