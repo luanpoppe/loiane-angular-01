abcee7f20a076dac0d526d19c6705b6bba037c14
luanpoppe <luanpoppe@gmail.com>
2023-09-14 13:58:41 -0300

    Terminei a aula 'Rotas: Tela de Login e como não mostrar o Menu (NavBar)'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { AuthService } from './login/auth.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'projeto-02';
+
+  mostrarMenu: boolean = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.mostrarMenuEmitter.subscribe(
+      (mostrar) => (this.mostrarMenu = mostrar)
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,11 +7,12 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosService } from './cursos/cursos.service';
 import { AppRoutingModule } from './app.routing.module';
+import { AuthService } from './login/auth.service';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [CursosService],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [CursosService, AuthService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
