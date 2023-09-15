cf0c8b5a330d57daa9986076eb1c2183fefe77c1
luanpoppe <luanpoppe@gmail.com>
2023-09-15 12:05:56 -0300

    Terminei a aula 'Usando Guarda de Rotas: CanLoad: como não carregar o módulo sem permissão'

--- src/app/guards/auth-guard.service.ts
+++ src/app/guards/auth-guard.service.ts
@@ -1,7 +1,10 @@
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  CanLoad,
+  Route,
   Router,
   RouterStateSnapshot,
 } from '@angular/router';
@@ -10,13 +13,17 @@ import { AuthService } from '../login/auth.service';
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
   constructor(private authAService: AuthService, private router: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean {
+    return this.verificarAcesso();
+  }
+
+  verificarAcesso() {
     if (this.authAService.usuarioEstaAutenticado()) {
       return true;
     }
@@ -24,5 +31,9 @@ export class AuthGuard implements CanActivate {
     this.router.navigate(['/login']);
 
     return false;
+  }
+
+  canLoad(route: Route): Observable<boolean> | Promise<boolean> | boolean {
+    return this.verificarAcesso();
   }
 }
