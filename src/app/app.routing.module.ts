fd55da17077c8af4e1fa8105c124b02d349858bc
luanpoppe <luanpoppe@gmail.com>
2023-09-15 13:34:22 -0300

    Terminei a aula 'Estilo de url: HTML5 ou usando #'

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -36,7 +36,11 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [
+    RouterModule.forRoot(appRoutes, {
+      useHash: true,
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
