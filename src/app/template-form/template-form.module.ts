59dad3fde2cf6081ffd6a3cf0cc10909b035e747
luanpoppe <luanpoppe@gmail.com>
2023-09-18 00:20:26 -0300

    Terminei a aula ‘Forms (Dica): Verificando dados do Form no template com JSON’

--- src/app/template-form/template-form.module.ts
+++ src/app/template-form/template-form.module.ts
@@ -2,9 +2,10 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TemplateFormComponent } from './template-form.component';
+import { FormDebugComponent } from '../form-debug/form-debug.component';
 
 @NgModule({
-  declarations: [TemplateFormComponent],
+  declarations: [TemplateFormComponent, FormDebugComponent],
   imports: [CommonModule, FormsModule],
 })
 export class TemplateFormModule {}
