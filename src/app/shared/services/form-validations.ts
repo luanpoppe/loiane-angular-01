40d0ce28a289cd8e2d323a9315a01f644b47c373
luanpoppe <luanpoppe@gmail.com>
2023-09-19 14:05:39 -0300

    Terminei a aula ‘Formulários reativos: Validação entre dois campos (confirmar email)’

--- src/app/shared/services/form-validations.ts
+++ src/app/shared/services/form-validations.ts
@@ -1,4 +1,4 @@
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 export class FormValidations {
   static requiredMinCheckbox(min = 1): any {
@@ -22,5 +22,27 @@ export class FormValidations {
     }
 
     return null;
+  }
+
+  static equalsTo(otherField: string) {
+    const validator = (formControl: FormControl) => {
+      if (otherField == null) {
+        throw new Error('É necessário informar um campo');
+      }
+
+      if (!formControl.root || !(<FormGroup>formControl.root).controls) {
+        return null;
+      }
+      const field = (<FormGroup>formControl.root).get(otherField);
+
+      if (!field) {
+        throw new Error('É necessário informar um campo válido');
+      }
+      if (field.value !== formControl.value) {
+        return { equalsTo: otherField };
+      }
+      return null;
+    };
+    return validator;
   }
 }
