142f2b5796d0fff4ee7502c234874782cc82f6b2
luanpoppe <luanpoppe@gmail.com>
2023-09-21 15:51:23 -0300

    Terminei a aula ‘Upload Arquivo: Barra de Progresso + Observando Eventos Http’

--- src/app/upload-file/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UploadFileService } from '../upload-file.service';
+import { HttpEvent, HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-upload-file',
@@ -8,6 +9,8 @@ import { UploadFileService } from '../upload-file.service';
 })
 export class UploadFileComponent implements OnInit {
   files: Set<File>;
+  progress: number = 0;
+
   constructor(private service: UploadFileService) {}
 
   ngOnInit(): void {}
@@ -24,14 +27,26 @@ export class UploadFileComponent implements OnInit {
       fileNames.push(selectedFiles[i].name);
       this.files.add(selectedFiles[i]);
     }
-    // document.getElementById('customFileLabel').innerHTML = fileNames.join(', ');
+    document.getElementById('customFileLabel').innerHTML = fileNames.join(', ');
+    this.progress = 0;
   }
 
   onUpload() {
     if (this.files && this.files.size > 0) {
       this.service
         .upload(this.files, '/api/upload')
-        .subscribe((response) => console.log('Upload concluído'));
+        .subscribe((event: HttpEvent<Object>) => {
+          // HttpEventType
+          console.log(event);
+          if (event.type === HttpEventType.Response) {
+            console.log('Upload Concluído');
+          } else if (event.type === HttpEventType.UploadProgress) {
+            const percentDone = Math.round((event.loaded * 100) / event.total);
+            console.log('Progresso', percentDone);
+            this.progress = percentDone;
+          }
+          console.log('Upload concluído');
+        });
     }
   }
 }
