aa149a43e927d12cde23c5bc6c436de1574b98a1
luanpoppe <luanpoppe@gmail.com>
2023-09-21 13:46:04 -0300

    Terminei a aula ‘Upload de Arquivo: Formulário Upload com Bootstrap 4’

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'cursos',
-  },
-  {
     path: 'cursos',
     loadChildren: () =>
       import('./cursos/cursos.module').then((m) => m.CursosModule),
+  },
+  {
+    path: 'upload',
+    loadChildren: () =>
+      import('./upload-file/upload-file.module').then(
+        (m) => m.UploadFileModule
+      ),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'upload',
   },
 ];
 
