9d987eb1cda1d6e90cdbe5f80903a76d54ca95da
luanpoppe <luanpoppe@gmail.com>
2023-09-22 11:47:54 -0300

    Terminei a aula ‘Diretivas: ngSwitch, ngSwitchCase e ngSwitchDefault’

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { DiretivaNgSwitchComponent } from './diretiva-ng-switch/diretiva-ng-switch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DiretivaNgifComponent
+    DiretivaNgifComponent,
+    DiretivaNgSwitchComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/diretiva-ng-switch/diretiva-ng-switch.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DiretivaNgSwitchComponent } from './diretiva-ng-switch.component';
+
+describe('DiretivaNgSwitchComponent', () => {
+  let component: DiretivaNgSwitchComponent;
+  let fixture: ComponentFixture<DiretivaNgSwitchComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DiretivaNgSwitchComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DiretivaNgSwitchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
