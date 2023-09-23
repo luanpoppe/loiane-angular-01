80116c0376097e9f5eff0efc9d7944dba8b9665f
luanpoppe <luanpoppe@gmail.com>
2023-09-23 11:38:11 -0300

    Terminei a aula ‘Diretivas: HostListener e HostBinding’

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { OperadorElvisComponent } from './operador-elvis/operador-elvis.componen
 import { ExemploNgcontentComponent } from './exemplo-ngcontent/exemplo-ngcontent.component';
 import { FundoAmareloDirective } from './shared/fundo-amarelo.directive';
 import { DiretivasCustomizadasComponent } from './diretivas-customizadas/diretivas-customizadas.component';
+import { HighlightMouseDirective } from './shared/highlight-mouse.directive';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { DiretivasCustomizadasComponent } from './diretivas-customizadas/diretiv
      ExemploNgcontentComponent,
      FundoAmareloDirective,
      DiretivasCustomizadasComponent,
+     HighlightMouseDirective,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
---
+++ src/app/shared/highlight-mouse.directive.ts
@@ -0,0 +1,34 @@
+import {
+  Directive,
+  HostListener,
+  ElementRef,
+  Renderer2,
+  HostBinding,
+} from '@angular/core';
+
+@Directive({
+  selector: '[HighlightMouse]',
+})
+export class HighlightMouseDirective {
+  @HostListener('mouseenter') onMouseOver() {
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   'background-color',
+    //   'yellow'
+    // );
+    this.backgroundColor = 'yellow';
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   'background-color',
+    //   'white'
+    // );
+    this.backgroundColor = 'white';
+  }
+
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+}
