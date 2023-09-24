236f02b39eac5664006ade49abf76ae4552d0302
luanpoppe <luanpoppe@gmail.com>
2023-09-24 14:30:42 -0300

    terminei a aula 'Aplicando Locale (internacionalização) nos Pipes'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import '@angular/common/locales/global/pt';
 
 import { AppComponent } from './app.component';
 import { ExamplesPipesComponent } from './examples-pipes/examples-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
+import { SettingsService } from './settings.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ExamplesPipesComponent,
-    CamelCasePipe
+  declarations: [AppComponent, ExamplesPipesComponent, CamelCasePipe],
+  imports: [BrowserModule],
+  providers: [
+    // { provide: LOCALE_ID, useValue: 'pt-BR' },
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: (settingsServices: any) => settingsServices.getLocale(),
+    },
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
